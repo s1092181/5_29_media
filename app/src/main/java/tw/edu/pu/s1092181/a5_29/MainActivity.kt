94a4f023de7c4cfd080cf4e0771d09cefd11ab52
s1092181 <kmes49@gmail.com>
2024-05-29 16:23:31 +0800

    v1

--- app/src/main/java/tw/edu/pu/s1092181/a5_29/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1092181/a5_29/MainActivity.kt
@@ -3,14 +3,22 @@ package tw.edu.pu.s1092181.a5_29
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.s1092181.a5_29.ui.theme._5_29Theme
 
@@ -33,25 +41,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    LazyColumn {
-        // Add a single item
+    var msg by remember { mutableStateOf("行動應用軟體開發") }
+    val animals = listOf(
+        R.drawable.animal0, R.drawable.animal1,
+        R.drawable.animal2, R.drawable.animal3,
+        R.drawable.animal4, R.drawable.animal5,
+        R.drawable.animal6, R.drawable.animal7,
+        R.drawable.animal8, R.drawable.animal9
+    )
+    val animalNames = listOf("鴨子", "企鵝", "青蛙", "貓頭鷹", "海豚", "牛", "無尾熊", "獅子", "狐狸", "小雞")
+
+    LazyRow(modifier = modifier.fillMaxSize()) {
+        itemsIndexed(List(20) { it }) { index, _ ->
+            Text(text = animalNames[index % 10])
+            Image(
+                painter = painterResource(id = animals[index % 10]),
+                contentDescription = "可愛動物圖片",modifier = Modifier.fillParentMaxWidth(1.0f)
+            )
+        }
         item {
-            Text(text = "Header")
-        }
-
-        // Add 3 items
-        items(51) { index ->
-            Text(text = "First List items : $index")
-        }
-
-        // Add 2 items
-        items(2) { index ->
-            Text(text = "Second List Items: $index")
-        }
-
-        // Add another single item
-        item {
-            Text(text = "Footer")
+            TextField(
+                value = msg,
+                onValueChange = { newValue ->
+                    msg = newValue
+                }
+            )
         }
     }
 }
